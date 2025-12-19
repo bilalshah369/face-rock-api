5d14356a65fec68d18e8ab841bb276bf7f70ecf2
bilalshah369 <bilalshah369@yahoo.com>
2025-12-19 16:29:42 +0530

    Remove dist, node_modules, and env files from git tracking

--- dist/utils/roles.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ROLES = void 0;
-exports.ROLES = {
-    NTA_ADMIN: 1,
-    DISPATCH: 2,
-    CENTRE_USER: 3,
-};
