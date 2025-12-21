83bde3a735bab088ff7f36a94d7d6567ec675f9d
bilalshah369 <bilalshah369@yahoo.com>
2025-12-21 19:26:32 +0530

    server catch

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,18 @@ import app from "./app";
 import db from "./config/db";
 const PORT = process.env.PORT || 3000;
 import path from "path";
-
+app.disable("etag");
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
+});
+app.use((req, res, next) => {
+  res.setHeader(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, proxy-revalidate"
+  );
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+  res.setHeader("Surrogate-Control", "no-store");
+  next();
 });
